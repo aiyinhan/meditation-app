ebfd350373090fae46a879ed98b130adc68843d3
hay <aiyinn0621@gmail.com>
2023-11-24 01:52:43 +0800

    Co-authored-by: WeiJie <WeiJie22@users.noreply.github.com>

--- lib/controllers/download_controller.dart
+++ lib/controllers/download_controller.dart
@@ -13,60 +13,93 @@ class DownloadController extends ChangeNotifier {
   final String songName;
   DownloadState downloadState = DownloadState.notDownloaded;
   final Completer<void> _downloadCompleter = Completer<void>();
-  List<String> downloadedSongs=[];
+  List<String> downloadedSongs = [];
+
   DownloadController(this.songUrl, this.songName);
+
+  CancelToken cancelToken = CancelToken();
+  double downloadProgress = 0.0;
+  String errorMessage = '';
 
   Future<void> startDownload() async {
     try {
       downloadState = DownloadState.downloading;
-      final songPath = await downloadAndSaveSong(songUrl, songName);
-      // if (downloadState == DownloadState.canceled) {
-      //   await deleteDownloadedSong(songPath);
-      // }
-      downloadState = DownloadState.downloaded;
-      // Add the downloaded song name to the list
-      downloadedSongs.add(songName);
-      print(downloadedSongs);
-      _downloadCompleter.complete();
+      final songPath = await downloadAndSaveSong(
+          songUrl, songName, cancelToken);
+
+      if (cancelToken.isCancelled) {
+        downloadState = DownloadState.canceled;
+        await _downloadCompleter.future;
+      } else {
+        downloadState = DownloadState.downloaded;
+        // Add the downloaded song name to the list
+        downloadedSongs.add(songName);
+        print(downloadedSongs);
+        _downloadCompleter.complete();
+      }
     } catch (e) {
       downloadState = DownloadState.failed;
-      _downloadCompleter.completeError(e);
+      //_downloadCompleter.completeError(e);
+      errorMessage = 'Download failed: $e';
     }
   }
 
   Future<void> cancelDownload() async {
     if (downloadState == DownloadState.downloading) {
+      cancelToken.cancel("Download canceled");
       downloadState = DownloadState.canceled;
+      print('Song download canceled');
       await _downloadCompleter.future;
+    }
+    downloadState = DownloadState.notDownloaded;
+  }
+
+
+  Future<String> _getFilePath(String songName) async {
+    final dir = await getApplicationDocumentsDirectory();
+    return "${dir.path}/$songName.mp3";
+  }
+
+  Future<void> downloadAndSaveSong(String songUrl, String songName,
+      CancelToken cancelToken) async {
+    Dio dio = Dio();
+    dio.options.headers["Range"] = "bytes=0-";
+    //Response response = await dio.get(songUrl, options: Options(responseType: ResponseType.bytes));
+    String path = await _getFilePath(songName);
+    Response response;
+    try {
+      response = await dio.download(
+        songUrl,
+        path,
+        cancelToken: cancelToken, // Pass the cancelToken to the request
+        onReceiveProgress: (receivedBytes, totalBytes) {
+          downloadProgress = receivedBytes / totalBytes;
+          notifyListeners();
+          print(downloadProgress);
+
+        },
+        deleteOnError: true,
+      );
+    } catch (e) {
+      if (e is DioException && CancelToken.isCancel(e)) {
+        // Handle the download cancellation here
+        return;
+      } else {
+        throw e;
+      }
     }
   }
 }
-
-Future<String> _getFilePath(String songName) async {
-  final dir = await getApplicationDocumentsDirectory();
-  return "${dir.path}/$songName.mp3";
-}
-
-Future<void> downloadAndSaveSong(String songUrl, String songName) async {
-  Dio dio = Dio();
-  double downloadProgress = 0.0;
-  //Response response = await dio.get(songUrl, options: Options(responseType: ResponseType.bytes));
-  String path = await _getFilePath(songName);
-  await dio.download(
-    songUrl,
-    path,
-    onReceiveProgress: (recivedBytes, totalBytes) {
-      downloadProgress = recivedBytes / totalBytes;
-      print(downloadProgress);
-    },
-    deleteOnError: true,
-  );
-}
-
-
-Future<void> deleteDownloadedSong(String songPath) async {
-  final songFile = File(songPath);
-  if (songFile.existsSync()) {
-    await songFile.delete();
+List<String> getDownloadedSongs() {
+  List<String> downloadedSongs = [];
+  Directory directory = Directory('/data/data/HanAiYin.meditation/app_flutter');
+  if (directory.existsSync()) {
+    List<FileSystemEntity> files = directory.listSync();
+    for (FileSystemEntity file in files) {
+      if (file is File) {
+        downloadedSongs.add(file.path);
+      }
+    }
   }
-}
+  return downloadedSongs;
+} //}
